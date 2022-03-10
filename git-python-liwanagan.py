2cc67c4e8f5c5759400e72f22d54e80e20d69813
LiwanaganShielaMae <gaddimae5@gmail.com>
2022-03-10 11:29:09 -0900

    My Hobbies added

--- git-python-liwanagan.py
+++ git-python-liwanagan.py
@@ -3,7 +3,12 @@ print("Malayak, Mogpog, Marinduque")
 print("January 25, 2002")
 print("Marinduque State College")
 print("Bachelor of Science in Information Technology")
-print("Second Year- Section D")        
+print("Second Year- Section D")  
+print("Watching horror/thriller movies, anime and asian dramas")
+print("Playing online games")
+print("Cooking or baking")
+print("Reading books, comics,and manga") 
+print("Drawing")     
 
 
         
