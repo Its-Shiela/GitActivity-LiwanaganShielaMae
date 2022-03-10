8f1937742e51dd1185e35002746f59b469c14b0b
LiwanaganShielaMae <gaddimae5@gmail.com>
2022-03-10 11:22:01 -0900

    My Educational Background added

--- git-python-liwanagan.py
+++ git-python-liwanagan.py
@@ -1,8 +1,9 @@
 print("Shiela Mae Gaddi Liwanagan")
 print("Malayak, Mogpog, Marinduque")
 print("January 25, 2002")
-
-        
+print("Marinduque State College")
+print("Bachelor of Science in Information Technology")
+print("Second Year- Section D")        
 
 
         
